2f340e2c554063867a652c2bdd0dd4ba10a13096
Monde Nel <Mondenel1996@gmail.com>
2025-03-25 14:01:53 +0200

    Set up React component to fetch and display contacts from Flask backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
 import React from "react"
+import { useState, useEffect } from "react"
 
 function App() {
-<>
-</>
+const [contacts, setContacts] = useState([])
+
+useEffect(() => {
+  fetchContacts()
+}, [])
+
+const fetchContacts = async () => {
+  const response = await fetch('http://127.0.0.1:5000/contacts')
+  const data = await response.json()
+  setContacts(data.contacts)
+  console.log(data.contacts);
+}
+
+return
+
+  <>
+    
+  </>
 }
 
 export default App
