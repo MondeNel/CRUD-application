6544f2be834fec3ec6a189a3795a7b84d12a901f
Monde Nel <Mondenel1996@gmail.com>
2025-03-26 14:35:54 +0200

    Created an object that contains a method that would post/create the content and convert it into json

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -14,6 +14,13 @@ const ContactForm = ({}) => {
             email
         }
         const url = "http://127.0.0.1:5000/create_account"
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify(data)
+        }
     }
 
 
